390acb5aed0352801e7dc4e1868f45bc6c0ae977
Hala <h7537281@gmail.com>
2026-08-23 02:36:40 +0300

    Configure PostgreSQL database and migrate schema

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.contrib.auth import get_user_model
+from api.models import Cart
+User = get_user_model()
+class OrderAPITestCase(APITestCase):
+    def setUp(self):
+        """تجهيز البيئة الافتراضية قبل تشغيل أي اختبار"""
+        # 1. إنشاء مستخدم تجريبي وتوثيق الهوية بضغطة واحدة
+        self.user = User.objects.create_user(username='testuser', password='password123')
+        self.client.force_authenticate(user=self.user)
+        
+        # 2. إنشاء سلة فارغة لاختبارها
+        self.cart = Cart.objects.create()
 
-# Create your tests here.
+    def test_create_order_with_empty_cart_fails(self):
+        """اختبار: التأكد من رفض تقديم طلب إذا كانت السلة فارغة"""
+        response = self.client.post('/api/orders/', {'cart_id': self.cart.id})
+        
+        # نتحقق أن الاستجابة هي 400 Bad Request
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
--- core/settings.py
+++ core/settings.py
@@ -82,11 +82,14 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ecommerec_db',
+        'USER': 'postgres',
+        'PASSWORD': '123456',  # استبدلي هذا بكلمة مرور حساب postgres لديكِ
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.1/ref/settings/#auth-password-validators
